62ff70226b7a749dbabd96fab0b2e2a7208ccf49
Rahul <rahulscodingworld@gmail.com>
2022-08-06 14:58:19 +0530

    finally mapped buffers, hash, and updated hash list

--- pages/upload-2.js
+++ pages/upload-2.js
@@ -4,8 +4,9 @@ import add from 'it-all'
 
 export default function Upload() {
     
-    const [buffer, setBuffer] = useState(null);
+    const [buffers, setbuffers] = useState(null);
     const [hash, setHash] = useState([]);
+    let newHash = []
 
     const  readAsArrayBuffer = (file) => {
         return new Promise((resolve, reject) => {
@@ -23,19 +24,27 @@ export default function Upload() {
         e.preventDefault();
         Promise.all(Array.from(e.target.files).map(readAsArrayBuffer))
         .then((urls) => {
-            setBuffer(urls)
+            setbuffers(urls)
         })
         .catch((error) => {
           console.error(error)
         })
     }
+
+ 
+    
     
    const  formSubmit = async (event) => {
         event.preventDefault();
-        console.log(buffer, 'buffer on submit')
-        const fileHash = await add(ipfs.addAll(buffer))
-        console.log(fileHash, 'file hash')
-    }
+        console.log(buffers[0], 'buffers on submit 1', buffers[2], 'buffers on submit 2')
+        for (let buffer of buffers) {
+            let filehash = await ipfs.add(buffer)
+            console.log(filehash.path)
+            newHash.push(filehash.path)
+            console.log(newHash)
+        }
+   }
+
         return (
             <div>
                 <h1>Upload to Files</h1>
