168f8945c26d9482aed6964d24fdde74298f1cf0
Rahul <rahulscodingworld@gmail.com>
2022-08-07 18:45:48 +0530

    kinda fixed rendering

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,85 @@
-import { useState } from 'react'
+import React, {useState} from 'react';
 import ipfs from "./api/ipfs";
+import add from 'it-all'
+import { render } from 'react-dom';
 
 export default function Home() {
-  const [buffer , setBuffer] = useState(null);
-  const [hash, setHash] = useState(null);
-  const [type, setType] = useState(false);
+    
+    const [buffers, setbuffers] = useState(null);
+    let typeOfFile = []
+    let newHash = []
+    let thingsToRender = []
 
-  const fileChange = (event) => {
-    event.preventDefault();
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = () => {
-      const currentBuffer = Buffer.from(reader.result);
-      console.log(currentBuffer)
-      setBuffer(currentBuffer);
+
+    const  readAsArrayBuffer = (file) => {
+        return new Promise((resolve, reject) => {
+          const fr = new FileReader()
+          fr.onerror = reject
+          fr.onload = function () {
+            resolve(fr.result)
+          }
+          fr.readAsArrayBuffer(file)
+        })
+      }
+
+
+   const  fileChange =  (e) => {
+        e.preventDefault();
+        Promise.all(Array.from(e.target.files).map(readAsArrayBuffer))
+        .then((urls) => {
+            setbuffers(urls)
+            console.log(urls, 'urls')
+            // for (let i = 0; i < urls.length; i++) {
+            //       if (fr['type'].split('/')[0] === 'image') {
+            //         setType(true);
+            //       } else {
+            //         setType(false);
+            //       }
+            // }
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     }
-    if (file['type'].split('/')[0] === 'image') {
-      setType(true);
-    } else {
-      setType(false);
-    }
-  }
 
-  const formSubmit = async (event) => {
-    event.preventDefault();
-    console.log(buffer.length);
-    const fileHash = await ipfs.add(buffer);
-    setHash(fileHash.path);
-  }
-
-  return (
-    <div>
-      <h1>Upload to Ipfs</h1>
-      <form onSubmit={formSubmit}>
-        <input type="file" onChange={fileChange} />
-      {!buffer ? <div>Please upload a file</div>: <input type='submit' />}
-      </form>
-      {!hash 
-      ? null 
-      : 
-      <div> 
-        {type 
-        ? <embed src={`https://ipfs.infura.io/ipfs/${hash}`} width='800px' height='500px' /> 
-        : 
-        <video width="560" height="315" preload='auto' controls>
-          <source src={`https://ipfs.infura.io/ipfs/${hash}`} type="video/mp4" />
-        </video>
+   const  formSubmit = async (event) => {
+        event.preventDefault();
+        // console.log(buffers[0], 'buffers on submit 1', buffers[2], 'buffers on submit 2')
+        for (let buffer of buffers) {
+            console.log('adding to ipfs')
+            let filehash = await ipfs.add(buffer)
+            console.log(filehash)
+            newHash.push(filehash.path)
+            console.log(newHash)
         }
+        renderFiles()
 
-          <div><a href={`https://ipfs.infura.io/ipfs/${hash}`}>Link</a></div> 
-      </div>}
-      <div>favicon</div>
-    </div>
-  )
+   }
 
+
+   const renderFiles = () => {
+    for (let i = 0; i < newHash.length; i++) {
+      // <video width="560" height="315" preload='auto' controls>
+      //         <source src={`https://ipfs.infura.io/ipfs/${totalHash[i]}`} type="video/mp4" />
+      // </video> 
+      console.log('rendering...')
+      thingsToRender.push(
+            <embed key={i} src={`https://ipfs.infura.io/ipfs/${newHash[i]}`} width='800px' height='500px' />
+
+      )
+      console.log(thingsToRender)
+    }
+   }
+
+
+        return (
+            <div>
+                <h1 className='noselect'>Upload to Files</h1>
+                <form onSubmit={formSubmit}>
+                    <input type="file" onChange={fileChange} multiple/>
+                    <input type='submit' />
+                </form>
+                <div>{!thingsToRender ?  thingsToRender : <div>loading..</div>}</div>
+            </div>
+        )                
 }
-
